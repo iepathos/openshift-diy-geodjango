894c6b826cd39b5f2a649cbbcb76f0a2b2dfc33b
Glen <iepathos@gmail.com>
2012-11-25 13:57:36 -0800

    Update .openshift/bootstrap.sh
    Updated all $OPENSHIFT_RUNTIME_DIR to $OPENSHIFT_DATA_DIR

--- .openshift/bootstrap.sh
+++ .openshift/bootstrap.sh
@@ -3,7 +3,7 @@
 export CFLAGS="-O3 -s"
 export CXXFLAGS="-O3 -s"
 export OPT="-O3 -s"
-export PATH=$OPENSHIFT_RUNTIME_DIR/bin:$PATH
+export PATH=$OPENSHIFT_DATA_DIR/bin:$PATH
 
 cd $OPENSHIFT_TMP_DIR
 
@@ -11,11 +11,11 @@ wget http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
 tar jxf Python-2.7.3.tar.bz2
 cd Python-2.7.3
 
-./configure --prefix=$OPENSHIFT_RUNTIME_DIR
+./configure --prefix=$OPENSHIFT_DATA_DIR
 make
 make install
 
-$OPENSHIFT_RUNTIME_DIR/bin/python -V
+$OPENSHIFT_DATA_DIR/bin/python -V
 
 cd $OPENSHIFT_TMP_DIR
 rm -rf ./Python-2.7.3*
@@ -23,7 +23,7 @@ rm -rf ./Python-2.7.3*
 wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz
 tar zxf setuptools-0.6c11.tar.gz
 cd setuptools-0.6c11
-$OPENSHIFT_RUNTIME_DIR/bin/python setup.py install
+$OPENSHIFT_DATA_DIR/bin/python setup.py install
 
 cd $OPENSHIFT_TMP_DIR
 rm -rf ./setuptools-0.6c11*
@@ -31,19 +31,19 @@ rm -rf ./setuptools-0.6c11*
 wget http://pypi.python.org/packages/source/p/pip/pip-1.1.tar.gz
 tar zxf pip-1.1.tar.gz
 cd pip-1.1
-$OPENSHIFT_RUNTIME_DIR/bin/python setup.py install
+$OPENSHIFT_DATA_DIR/bin/python setup.py install
 
 cd $OPENSHIFT_TMP_DIR
 rm -rf ./pip-1.1*
 
-$OPENSHIFT_RUNTIME_DIR/bin/pip install uwsgi
-$OPENSHIFT_RUNTIME_DIR/bin/uwsgi --version
+$OPENSHIFT_DATA_DIR/bin/pip install uwsgi
+$OPENSHIFT_DATA_DIR/bin/uwsgi --version
 
 cd $OPENSHIFT_TMP_DIR
 wget http://download.osgeo.org/proj/proj-4.8.0.tar.gz
 tar xzf proj-4.8.0.tar.gz
 cd proj-4.8.0
-./configure --prefix=$OPENSHIFT_RUNTIME_DIR
+./configure --prefix=$OPENSHIFT_DATA_DIR
 make
 make install
 cd ..
@@ -54,7 +54,7 @@ cd $OPENSHIFT_TMP_DIR
 wget http://download.osgeo.org/geos/geos-3.3.5.tar.bz2
 tar xjf geos-3.3.5.tar.bz2
 cd geos-3.3.5
-./configure --prefix=$OPENSHIFT_RUNTIME_DIR
+./configure --prefix=$OPENSHIFT_DATA_DIR
 make
 make install
 cd ..
@@ -65,7 +65,7 @@ cd $OPENSHIFT_TMP_DIR
 wget ftp://ftp.remotesensing.org/gdal/gdal-1.9.1.tar.gz
 tar zxf gdal-1.9.1.tar.gz
 cd gdal-1.9.1
-./configure --prefix=$OPENSHIFT_RUNTIME_DIR --disable-static --with-geos=$OPENSHIFT_RUNTIME_DIR/bin/geos-config
+./configure --prefix=$OPENSHIFT_DATA_DIR --disable-static --with-geos=$OPENSHIFT_DATA_DIR/bin/geos-config
 make
 make install
 cd ..
